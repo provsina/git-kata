829ceefc19d4e19e5cfff808d362ae1a14180674
Dan Siwiec <daniel.siwiec@gmail.com>
2016-03-20 23:51:36 -0700

    First kata - stashing files

---
+++ kata1.sh
@@ -0,0 +1,8 @@
+echo "KATA 1 - stashing files"
+echo
+echo "SCENARIO DESCRIPTION"
+echo "You made local changes to some files. In order to pull
+the most recent changes from the remote repository,
+you have to stash those local changes"
+
+echo "console.log('sky is the limit')" >> src/app.js
---
+++ src/app.js
@@ -0,0 +1,3 @@
+let string = 'hello world'
+
+console.log(string)
