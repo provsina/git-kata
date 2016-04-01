00adc46602d8eca1e2c91acd0913094cf8767b77
Dan Siwiec <daniel.siwiec@gmail.com>
2016-03-31 18:27:05 -0700

    Remove branches - all katas in master

---
+++ kata1.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+commitWithJohnny() {
+  cd .johnny
+  echo "console.log('I am johnny')" > src/johnny.js
+  git add .
+  git commit -m "Johnny's commit"
+  git push
+  cd ..
+}
+
+editFile() {
+  echo "console.log('sky is the limit')" >> src/app.js
+}
+
+echo "KATA 1 - STASHING FILES"
+echo
+echo "SCENARIO DESCRIPTION"
+echo "You made local changes to some files. Pull remote
+changes without committing your local changes
+
+commitWithJohnny > /dev/null 2>&1
+editFile > /dev/null 2>&1
